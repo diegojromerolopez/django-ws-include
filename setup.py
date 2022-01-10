a1df9e9f7f4df663fef9832a90543d8ca7f9b5ff
Diego J <diegojromerolopez@gmail.com>
2022-01-10 11:57:08 +0100

    Fix setup.py data_files

--- setup.py
+++ setup.py
@@ -6,24 +6,13 @@ root_dir_path = os.path.dirname(os.path.abspath(__file__))
 long_description = open(os.path.join(root_dir_path, "README.md")).read()
 templates_dir_relative_path = './ws_include/templates'
 
-data_files = []
-for dirpath, dirnames, filenames in os.walk("."):
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith("."):
-            del dirnames[i]
-    if "__init__.py" in filenames:
-        continue
-    elif filenames:
-        data_files.append(
-            [dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
 setup(
     name="django-ws-include",
     version="0.1.0",
     author="Diego J. Romero López",
     author_email="diegojromerolopez@gmail.com",
     description=(
-        "A simple application for Django to include "
+        "A simple application for Django to include " +
         "(and fetch) asynchronous templates by loading templates with websocket connections."
     ),
     long_description=long_description,
@@ -37,7 +26,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries"
     ],
     install_requires=[
         "channels>=3.0.4",
@@ -53,7 +42,7 @@ setup(
             templates_dir_relative_path, (
                 os.path.join(templates_dir_relative_path, "ws_include/spinner.html"),
                 os.path.join(templates_dir_relative_path, "ws_include/template_tag.html"),
-                os.path.join(templates_dir_relative_path, "ws_include/template_tag.html")
+                os.path.join(templates_dir_relative_path, "ws_include/template_tag.js")
             )
         )
     ],
